792fa406b65806f2454250414218646a24c82a36
mana <karahara223@gmail.com>
2020-07-31 17:48:22 +0900

    second

--- src/main/java/mavenTest/MavenHello.java
+++ src/main/java/mavenTest/MavenHello.java
@@ -1,12 +1,31 @@
+
 package mavenTest;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+import org.junit.Test;
+
 
 public class MavenHello {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
-		System.out.println("Maven Hello!!");
-
-	}
+    @Test
+    public void testGetColor1() {
+        Color1 c1 = new Color1();
+        String t1 = c1.getColor(1);
+        assertThat(t1,is("赤"));
+    }
+    @Test
+    public void testGetColor2() {
+        Color1 c1 = new Color1();
+        String t1 = c1.getColor(2);
+        assertThat(t1,is("青"));
+    }
+    @Test
+    public void testGetColor3() {
+        Color1 c1 = new Color1();
+        String t1 = c1.getColor(3);
+        assertThat(t1,is("1or2を入力して下さい"));
+    }
 
 }
